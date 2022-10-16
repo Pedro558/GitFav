ce986b2d142a216f99d0e3a98cfbb14ce1e95800
pedro558 <pedroafonso109@gmail.com>
2022-10-16 14:43:19 -0300

    :construction: Add Favorite class

--- js/favorites.js
+++ js/favorites.js
@@ -15,3 +15,56 @@ export class GithubUser{
     })
   }
 }
+
+// How the data will be structured
+export class Favorites{
+  constructor(root){
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  // load what is already saved in localStorage
+  load(){
+    this.inputs = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  // save data into localStorage
+  saveLocally(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.inputs))
+  }
+
+  // dealing with data when a new user is added 
+  async addUser(username){
+    const error = document.querySelector('.error')
+    
+    try{
+      // map the users already in the list
+      const loginNames = this.inputs.map(user => {
+        return user.login.toLowerCase()
+      })
+
+      // check if a user is already in the list
+      for(let i = 0; i < loginNames.length; i++){
+        if(username == loginNames[i]){
+          error.classList.add('open')
+          error.firstElementChild.innerHTML = 'User already added'
+        }
+      }
+
+      // load API's data into a const
+      const user = await GithubUser.search(username)
+
+      // check if the user is find
+      if(user.login === undefined){
+        error.classList.add('open')
+        error.firstElementChild.innerHTML = 'User not find'
+      }
+
+      // remove error message from the screen
+      error.classList.remove('open')
+
+      
+
+    }
+  }
+}
